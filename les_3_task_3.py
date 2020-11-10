1ddf8cf41be1538eb30192f57ef322660d818491
Vladislav Ruban <63507131+Fristl@users.noreply.github.com>
2020-11-10 14:31:13 +0300

    Update les_3_task_3.py

--- les_3_task_3.py
+++ les_3_task_3.py
@@ -11,10 +11,10 @@ array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 
 
-min_el = MAX_ITEM
-max_el = MIN_ITEM
+min_el = float('inf')      # MAX_ITEM
+max_el = -float('inf')     # MIN_ITEM
 idx_min = 0
-idx_max = SIZE - 1
+idx_max = len(array) - 1   # SIZE - 1
 
 for idx, item in enumerate(array):
     if item > max_el:
